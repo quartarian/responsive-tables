6c2f037380947b61c1821b474fd2eca4df408cfa
Matthew Bender <quartarian@gmail.com>
2016-02-24 09:24:34 -0500

    Update javascript to work in Jquery no conflict mode.

--- responsive-tables.js
+++ responsive-tables.js
@@ -1,46 +1,46 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
   var switched = false;
   var updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
+    if ((jQuery(window).width() < 767) && !switched ){
       switched = true;
-      $("table.responsive").each(function(i, element) {
-        splitTable($(element));
+      jQuery("table.responsive").each(function(i, element) {
+        splitTable(jQuery(element));
       });
       return true;
     }
-    else if (switched && ($(window).width() > 767)) {
+    else if (switched && (jQuery(window).width() > 767)) {
       switched = false;
-      $("table.responsive").each(function(i, element) {
-        unsplitTable($(element));
+      jQuery("table.responsive").each(function(i, element) {
+        unsplitTable(jQuery(element));
       });
     }
   };
    
-  $(window).load(updateTables);
-  $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
-  $(window).on("resize", updateTables);
+  jQuery(window).load(updateTables);
+  jQuery(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
+  jQuery(window).on("resize", updateTables);
    
-	
-	function splitTable(original)
-	{
-		original.wrap("<div class='table-wrapper' />");
-		
-		var copy = original.clone();
-		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
+  
+  function splitTable(original)
+  {
+    original.wrap("<div class='table-wrapper' />");
+    
+    var copy = original.clone();
+    copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
+    copy.removeClass("responsive");
+    
+    original.closest(".table-wrapper").append(copy);
+    copy.wrap("<div class='pinned' />");
+    original.wrap("<div class='scrollable' />");
 
     setCellHeights(original, copy);
-	}
-	
-	function unsplitTable(original) {
+  }
+  
+  function unsplitTable(original) {
     original.closest(".table-wrapper").find(".pinned").remove();
     original.unwrap();
     original.unwrap();
-	}
+  }
 
   function setCellHeights(original, copy) {
     var tr = original.find('tr'),
@@ -48,11 +48,11 @@ $(document).ready(function() {
         heights = [];
 
     tr.each(function (index) {
-      var self = $(this),
+      var self = jQuery(this),
           tx = self.find('th, td');
 
       tx.each(function () {
-        var height = $(this).outerHeight(true);
+        var height = jQuery(this).outerHeight(true);
         heights[index] = heights[index] || 0;
         if (height > heights[index]) heights[index] = height;
       });
@@ -60,7 +60,7 @@ $(document).ready(function() {
     });
 
     tr_copy.each(function (index) {
-      $(this).height(heights[index]);
+      jQuery(this).height(heights[index]);
     });
   }
 
